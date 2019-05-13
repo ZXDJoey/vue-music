8ca1f65c983ca8c5b7b3e4a0c04d55e832a27ca9
zxdjoey <774793337@qq.com>
2019-05-13 22:37:17 +0800

    update

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -8,12 +8,28 @@
 import { mapGetters } from 'vuex'
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
+  components: {
+    MusicList
+  },
+  data () {
+    return {
+      songs: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
-    ])
+    ]),
+    title () {
+      return this.singer.name
+    },
+    bgImage () {
+      return this.singer.avatar
+    }
   },
   created () {
     this._getDetail()
@@ -26,9 +42,19 @@ export default {
       }
       getSingerDetail(this.singer.id).then(res => {
         if (res.code === ERR_OK) {
-          console.log(this.singer.id)
+          this.songs = this._normalizeSongs(res.data.list)
         }
       })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach(item => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   }
 }
@@ -37,14 +63,6 @@ export default {
 <style lang="stylus" scoped>
   @import "~common/stylus/variable"
 
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
   .slide-enter-active, .slide-leave-active
     transition : all 0.3s
   .slide-enter, .slide-leave-to
