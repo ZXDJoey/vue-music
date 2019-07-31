71f9079a9a84f0746dcb99b77ce26e86887e55e2
zhangxiaodong <774793337@qq.com>
2019-07-31 15:59:27 +0800

    Update getters.js

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,7 +5,4 @@ export const playList = state => state.playList
 export const sequenceList = state => state.sequenceList
 export const mode = state => state.mode
 export const currentIndex = state => state.currentIndex
-
-export const currentSong = (state) => {
-  return state.playList[state.currentSong] || {}
-}
+export const currentSong = state => state.playList[state.currentIndex] || {}
