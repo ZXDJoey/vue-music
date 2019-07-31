704057ef843fc21f1858edd7bd7b75c4a024eb15
zhangxiaodong <774793337@qq.com>
2019-07-31 10:22:37 +0800

    Update state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import { playMode } from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {}, // 歌手集合
+  playing: false, // 播放
+  fullScreen: false, // 全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 顺序播放列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1 // 当前播放索引
 }
 
 export default state
